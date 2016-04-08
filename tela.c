58b7c2225fc4fad28ecc0c730fad4dd788e53c0b
fabiosumida <dnxktzy@hotmail.com>
2016-04-08 00:46:43 -0300

    comentarios na tela.c
    primeiro commit Sumida

--- tela.c
+++ tela.c
@@ -44,7 +44,7 @@ void *telaRun(void *param) {
 	}
 }
 
-
+//funçao para escrever na tela
 void writeTela(char *str, int lin, int col) {
 	if(lin >= telaLinha || col >= telaColuna) {
 		// não escreve na parte da tela q não foi alocada
@@ -60,22 +60,25 @@ void writeTela(char *str, int lin, int col) {
 	}
 }
 
-
+//funçao para executar a função 'imprimirPontoOnibus' para todos os onibus
 void imprimirTodosPontosOnibus() {
 	for(int i=0; i<S; i++) {
 		imprimirPontoOnibus(&pontosOnibus[i]);
 	}
 }
 
-
+//função para imprimir os dados do ponto de onibus como id do ponto, passageiro na fila de espera e onibus que esta no ponto
 void imprimirPontoOnibus(pontoOnibus_t *this) {
 	char buffer[1000];
 
 	// [S00 100]
+	//escreve na variavel buffer
 	sprintf(buffer, "[S%02d %03d]", this->id, this->passageiros->length);
+	//escreve na tela no local adequado
 	writeTela(buffer, this->id * 4, 0);
 
 	// [C00]
+	//se tiver onibus neste ponto imprime o id dele, se não tiver imprime apenas colchetes vazios para indicar que não há onibus neste ponto
 	if(this->onibus) {
 		sprintf(buffer, "[C%02d]", this->onibus->id);
 		writeTela(buffer, this->id * 4 + 2, 1);
@@ -85,14 +88,17 @@ void imprimirPontoOnibus(pontoOnibus_t *this) {
 		writeTela("[   ]", this->id * 4 + 2, 1);
 	}
 
+
 	int i = 0;
 	passageiro_t *passageiro;
 
-		
+	//percorre todos os passageiros neste ponto 	
 	forList(passageiro_t *, passageiro, this->passageiros) {
+		//não imprime mais que 30 passageiros, apesar de inseri-los no ponto onibus
 		if (i==30){
 			break;
 		}
+		//imprime o passageiro no local adequado
 		sprintf(buffer, "%03d", passageiro->id);
 		writeTela(buffer, (this->id * 4) + i/10, 10 + 4 * (i % 10));
 		i++;
@@ -100,31 +106,40 @@ void imprimirPontoOnibus(pontoOnibus_t *this) {
 
 }
 
+//funçao para executa a funçao imprimirOnibus para todos os onibus
 void imprimirTodosOnibus() {
 	for(int i=0; i<C; i++) {
 		imprimirOnibus(&onibusArray[i]);
 	}
 }
 
+//funçao para imprimir os dados do ponto de onibus como por exemplo id do onibus, situaçao do onibus (parado ou andando), onde esta, para onde vai e passageiro no onibus
 void imprimirOnibus(onibus_t *this) {
 	char buffer[1000];
 
 	//[C00 99]
+	//escreve no buffer o id do onibus e o numero de passageiros no onibus
 	sprintf(buffer, "[C%02d %03d]", this->id, this->passageiros->length);
 	#define colunaOnibus 55
+	//escreve na tela o que esta no buffer
 	writeTela(buffer, this->id * 4, colunaOnibus);
 	passageiro_t *passageiro;
 	int i = 0;
 
+	//percorre os passageiros do onibus
 	forList(passageiro_t*, passageiro, this->passageiros) {
+		//nao imprime mais que 30 passageiros, apesar de inseri-los no onibus
 		if (i==30){
 			break;
 		}
+		//imprime os passageiros
 		sprintf(buffer, "%03d", passageiro->id);
 		writeTela(buffer, this->id * 4 + i/10, colunaOnibus + 10 + 4 * (i % 10));
 		i++;
 	}
 
+	//se o onibus estiver parado num ponto de onibus imprime == antes do id do ponto, senão imprime >> antes do id do ponto
+	//sao impressos no local adequado
 	if (this->pontoOnibus){
 		sprintf(buffer, "== [S%02d]", this->pontoOnibus->id);
 	}
